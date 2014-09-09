4c7ed6e06d447e987b9b01a93211f064ea15d431
Dhany Alfian <dhany@bilna.com>
2014-09-09 15:09:00 +0700

    - form basics (done)

--- application/views/forms/basic_forms_view.php
+++ application/views/forms/basic_forms_view.php
@@ -1,139 +1,152 @@
 <div class="row-panel">
-    <div class="col-md-12 heading_title">
+    <div class="heading_title">
         Form Elements
     </div>
     
-    <div class="col-md-6 panel-left">
-        <form role="form">
-            <div class="form-group">
-                <label for="textInput">Text Input</label>
-                <input type="text" class="form-control" id="textInput" placeholder="Text Input" />
+    <form role="form" action="#">
+        <div class="form-group">
+            <label for="input-text">Text</label>
+            <input type="email" class="form-control" id="input-text" placeholder="Text" />
+        </div>
+
+        <div class="form-group">
+            <label for="input-email">Email address</label>
+            <input type="email" class="form-control" id="input-email" placeholder="Enter email" />
+        </div>
+
+        <div class="form-group">
+            <label for="input-password">Password</label>
+            <input type="password" class="form-control" id="input-password" placeholder="Password" />
+        </div>
+
+        <div class="form-group">
+            <label for="textarea">Textarea</label>
+            <textarea class="form-control" id="textarea" rows="3"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="input-select">Select</label>
+            <select class="form-control" id="input-select">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="input-select-multiple">Select (Multiple)</label>
+            <select multiple class="form-control" id="input-select-multiple">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="input-file">File input</label>
+            <input type="file" id="input-file">
+            <p class="help-block">Example block-level help text here.</p>
+        </div>
+
+        <div class="row-panel">
+            <label>Checkbox (stacked)</label>
+            <div class="checkbox">
+                <label><input type="checkbox" /> Option 1</label>
             </div>
-            
-            <div class="form-group">
-                <label for="textArea">Textarea</label>
-                <textarea class="form-control" id="textArea"></textarea>
+            <div class="checkbox">
+                <label><input type="checkbox" /> Option 2</label>
             </div>
-            
-            <div class="row">
-                <div class="col-lg-6">
-                    <label>Checkbox (stacked)</label>
-                    <div class="checkbox">
-                        <label><input type="checkbox" /> Option 1</label>
-                    </div>
-                    <div class="checkbox">
-                        <label><input type="checkbox" /> Option 2</label>
-                    </div>
-                </div>
-                
-                <div class="col-lg-6">
-                    <label>Radio (stacked)</label>
-                    <div class="radio">
-                        <label><input type="radio" name="radio" /> Option 1</label>
-                    </div>
-                    <div class="radio">
-                        <label><input type="radio" name="radio" /> Option 2</label>
-                    </div>
-                </div>
+        </div>
+
+        <div class="row-panel">
+            <label>Radio (stacked)</label>
+            <div class="radio">
+                <label><input type="radio" name="radio" /> Option 1</label>
             </div>
-            
-            <div class="btn-group">
-                <button class="btn btn-default btn-sm" type="button">
-                    Submit
-                </button>
-                
-                <button class="btn btn-default btn-sm" type="reset">
-                    Reset
-                </button>
+            <div class="radio">
+                <label><input type="radio" name="radio" /> Option 2</label>
             </div>
-        </form>
-    </div>
-    
-    <div class="col-md-6 panel-right">
-        <form role="form">
-            <div class="form-group">
-                <label for="select">Select</label>
-                <select class="form-control" id="select">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            
-            <div class="form-group">
-                <label for="selectMultiple">Select (Multiple)</label>
-                <select multiple class="form-control" id="selectMultiple">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            
-            <div class="form-group">
-                <label for="exampleInputFile">File input</label>
-                <input type="file" id="exampleInputFile" />
-                <p class="help-block">Example block-level help text here.</p>
-            </div>
-        </form>
-    </div>
-    
-    <div class="clearfix"></div>
+        </div>
+
+        <div class="row-panel btn-theme">
+            <button type="submit" class="btn btn-default btn-sm">Submit</button>
+        </div>
+    </form>
 </div>
 
 <div class="row-panel">
-    <div class="col-md-12 heading_title">
+    <div class="heading_title">
         Validations
     </div>
     
-    <div class="col-md-6 panel-left">
-        <form role="form">
-            <div class="form-group has-success">
-                <input type="text" class="form-control" id="textInput" placeholder="Text Input" />
-                <span class="help-block">Success</span>
+    <form role="form">
+        <div class="form-group has-success">
+            <input type="text" class="form-control" id="textInput" placeholder="Text Input" />
+            <span class="help-block">Success</span>
+        </div>
+
+        <div class="form-group has-warning">
+            <input type="text" class="form-control" id="textInput" placeholder="Text Input" />
+            <span class="help-block">Warning</span>
+        </div>
+
+        <div class="form-group has-error">
+            <input type="text" class="form-control" id="textInput" placeholder="Text Input" />
+            <span class="help-block">Error</span>
+        </div>
+
+        <div class="has-error">
+            <div class="radio">
+                <label class="checkbox">
+                    <input type="checkbox" id="checkboxError" value="option1">
+                    Option one is this and that&mdash;be sure to include why it's great
+                </label>
             </div>
-            
-            <div class="form-group has-warning">
-                <input type="text" class="form-control" id="textInput" placeholder="Text Input" />
-                <span class="help-block">Warning</span>
+        </div>
+
+        <div class="has-success">
+            <div class="radio">
+                <label>
+                    <input type="radio" name="optionsRadios" id="radiosSuccess1" value="option1" checked>
+                    Option one is this and that&mdash;be sure to include why it's great
+                </label>
             </div>
-            
-            <div class="row">
-                <div class="col-lg-6">
-                    <label>Checkbox (stacked)</label>
-                    <div class="checkbox">
-                        <label><input type="checkbox" /> Option 1</label>
-                    </div>
-                    <div class="checkbox">
-                        <label><input type="checkbox" /> Option 2</label>
-                    </div>
-                </div>
-                
-                <div class="col-lg-6">
-                    <label>Radio (stacked)</label>
-                    <div class="radio">
-                        <label><input type="radio" name="radio" /> Option 1</label>
-                    </div>
-                    <div class="radio">
-                        <label><input type="radio" name="radio" /> Option 2</label>
-                    </div>
-                </div>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="optionsRadios" id="radiosSuccess2" value="option2">
+                    Option two can be something else and selecting it will deselect option one
+                </label>
             </div>
-            
-            <div class="btn-group">
-                <button class="btn btn-default btn-sm" type="button">
-                    Submit
-                </button>
-                
-                <button class="btn btn-default btn-sm" type="reset">
-                    Reset
-                </button>
-            </div>
-        </form>
+        </div>
+    </form>
+</div>
+
+<div class="row-panel">
+    <div class="heading_title">
+        With optional icons
     </div>
     
-    <div class="clearfix"></div>
+    <form role="form">
+        <div class="form-group has-success has-feedback">
+            <label class="control-label" for="inputSuccess2">Input with success</label>
+            <input type="text" class="form-control" id="inputSuccess2">
+            <span class="glyphicon glyphicon-ok form-control-feedback"></span>
+        </div>
+        
+        <div class="form-group has-warning has-feedback">
+            <label class="control-label" for="inputWarning2">Input with warning</label>
+            <input type="text" class="form-control" id="inputWarning2">
+            <span class="glyphicon glyphicon-warning-sign form-control-feedback"></span>
+        </div>
+        
+        <div class="form-group has-error has-feedback">
+            <label class="control-label" for="inputError2">Input with error</label>
+            <input type="text" class="form-control" id="inputError2">
+            <span class="glyphicon glyphicon-remove form-control-feedback"></span>
+        </div>
+    </form>
 </div>
